e85687c37dda57e8d43751d2dee32b06a3c392c5
Trond A Ekseth <troeks@gmail.com>
2024-05-25 11:18:50 +0200

    Set watch to be recursive

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,6 +1,6 @@
 import { watch } from "node:fs";
 import { build } from "./build";
 
-watch("src", async (_event, _filename) => {
+watch("src", { recursive: true }, async (_event, _filename) => {
   await build();
 });
