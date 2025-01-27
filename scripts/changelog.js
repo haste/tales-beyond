8f282dd99af776c34d6b2f218a6b498be1e8309d
Trond A Ekseth <troeks@gmail.com>
2025-01-27 17:44:10 +0100

    Include changelog when submitting to AMO

---
+++ scripts/changelog.js
@@ -0,0 +1,66 @@
+import Bun from "bun";
+import { marked } from "marked";
+
+const [_exec, _script, format, input] = Bun.argv;
+
+const mozillaRenderer = {
+  heading({ tokens }) {
+    const text = this.parser.parseInline(tokens);
+
+    return `<strong>${text}</strong>\n`;
+  },
+};
+
+const modioRenderer = {
+  heading({ tokens }) {
+    return `${this.parser.parseInline(tokens)}\n`;
+  },
+
+  list({ items }) {
+    const body = [];
+    for (const item of items) {
+      body.push(this.listitem(item));
+    }
+    return `${body.join("")}\n`;
+  },
+
+  listitem({ tokens }) {
+    return ` - ${this.parser.parseInline(tokens)}\n`;
+  },
+
+  text(token) {
+    return token.tokens ? this.parser.parseInline(token.tokens) : token.text;
+  },
+};
+
+const md = await Bun.file(input).text();
+
+const offsets = [...md.matchAll(/## (?<version>\S+) \((?<date>[^\)]+)\)/g)].map(
+  (match) => ({
+    index: match.index,
+    version: match.groups.version,
+    date: match.groups.date,
+  }),
+);
+
+const latestEntry = md.slice(
+  md.indexOf("### ", offsets[1].index),
+  offsets[2].index - 2,
+);
+
+if (format === "amo") {
+  marked.use({ renderer: mozillaRenderer });
+
+  console.log(
+    JSON.stringify({
+      version: {
+        release_notes: {
+          "en-US": marked.parse(latestEntry).trim(),
+        },
+      },
+    }),
+  );
+} else if (format === "modio") {
+  marked.use({ renderer: modioRenderer });
+  console.log(marked.parse(latestEntry).trim());
+}
