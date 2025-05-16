fee3cb9d9fe79e4ed2188892493190950da4bedc
Uriah Brown <ubrown@igsenergy.com>
2025-05-16 13:36:06 -0400

    make release os agnostic

--- scripts/release.js
+++ scripts/release.js
@@ -3,7 +3,9 @@
 // Creates chrome and symbiote zips
 
 import path from "node:path";
+import fs from "node:fs/promises";
 import Bun, { $ } from "bun";
+import AdmZip from "adm-zip";
 
 await $`bun web-ext build -o -s build/chrome`;
 
@@ -13,6 +15,15 @@ const { version } = await Bun.file(
 
 const symbioteZip = `web-ext-artifacts/tales-beyond-symbiote-${version}.zip`;
 
-await $`rm -f ${symbioteZip}`;
-await $`7z a ${symbioteZip} ./build/symbiote/`;
-await $`7z rn ${symbioteZip} symbiote tales-beyond`;
+try {
+    await fs.unlink(symbioteZip);
+} catch (err) {
+    if (err.code !== "ENOENT") throw err;
+}
+
+const zip = new AdmZip();
+zip.addLocalFolder("./build/symbiote", "tales-beyond");
+
+await fs.mkdir("web-ext-artifacts", { recursive: true });
+
+zip.writeZip(symbioteZip);
